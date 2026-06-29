a1ea59f7fa1ffa18a2d0bcd22848696dde67d390
PedroAnjosPS <phpsanches@gmail.com>
2026-06-28 23:29:32 -0300

    Finalizando o contrato de salvar um Autor

--- src/main/java/io/github/cursodsousa/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/AutorController.java
@@ -1,21 +1,40 @@
 package io.github.cursodsousa.libraryapi.controller;
 
 import io.github.cursodsousa.libraryapi.controller.dto.AutorDTO;
-import org.springframework.http.HttpStatus;
+import io.github.cursodsousa.libraryapi.model.Autor;
+import io.github.cursodsousa.libraryapi.service.AutorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("autores")
 // http://localhost:8080/autores
 public class AutorController {
 
-    @PostMapping
-    public ResponseEntity salvar(@RequestBody AutorDTO autor) {
+    private final AutorService service;
 
-        return new ResponseEntity("Autor salvo com sucesso! " + autor, HttpStatus.CREATED);
+    public AutorController(AutorService service) {
+        this.service = service;
+    }
+    
+    @PostMapping
+    public ResponseEntity<Void> salvar(@RequestBody AutorDTO autor) {
+        Autor autorEntidade = autor.mapearParaAutor();
+        service.salvar(autorEntidade);
+
+        // Exemplo do que será retornado: http://localhost:8080/autores/76e7c418-ccf9-4e2a-af20-c28b9e50ab55
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(autorEntidade.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
